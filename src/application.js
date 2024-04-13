4b253ff4e877d1597f183a5dada4637720a33388
Alexander <nekon689@gmail.com>
2024-04-13 13:42:58 +0300

    fix

--- src/application.js
+++ src/application.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import onChange from 'on-change';
 
-export default () => {
-  const validateName = (name) => (name.trim().length ? [] : ['name cannot be empty']);
-  const validateEmail = (email) => (/\w+@\w+/.test(email) ? [] : ['invalid email']);
-  const validateField = (fieldname, data) => (fieldname === 'name' ? validateName(data) : validateEmail(data));
+const validateName = (name) => (name.trim().length ? [] : ['name cannot be empty']);
+const validateEmail = (email) => (/\w+@\w+/.test(email) ? [] : ['invalid email']);
+const validateField = (fieldname, data) => (fieldname === 'name' ? validateName(data) : validateEmail(data));
 
+export default () => {
   const state = {
     values: {
       name: '',
@@ -17,27 +17,28 @@ export default () => {
     },
   };
 
-  const fromContainer = document.querySelector('.form-container');
+  const formContainer = document.querySelector('.form-container');
 
-  const fromHTML = `
-    <form id="registrationForm">
-        <div class="form-group">
-            <label for="inputName">Name</label>
-            <input type="text" class="form-control" id="inputName" placeholder="Введите ваше имя" name="name" required>
-        </div>
-        <div class="form-group">
-            <label for="inputEmail">Email</label>
-            <input type="text" class="form-control" id="inputEmail" placeholder="Введите email" name="email" required>
-        </div>
-        <input type="submit" value="Submit" class="btn btn-primary">
-    </form>
-    `;
-  fromContainer.innerHTML = fromHTML;
+  const formHTML = `
+  <form id="registrationForm">
+    <div class="form-group">
+        <label for="inputName">Name</label>
+        <input type="text" class="form-control" id="inputName" placeholder="Введите ваше имя" name="name" required>
+    </div>
+    <div class="form-group">
+        <label for="inputEmail">Email</label>
+        <input type="text" class="form-control" id="inputEmail" placeholder="Введите email" name="email" required>
+    </div>
+    <input type="submit" value="Submit" class="btn btn-primary">
+  </form>
+  `;
+
+  formContainer.innerHTML = formHTML;
 
   const form = document.querySelector('form');
-  const sumbit = document.querySelector('[type="submit"]');
+  const submit = document.querySelector('[type="submit"]');
 
-  const watchState = onChange(state, (path) => {
+  const watchedState = onChange(state, (path) => {
     const selector = path.split('.')[1];
     const input = document.querySelector(`[name=${selector}]`);
     if (validateField(selector, state.values[selector]).length === 0) {
@@ -47,16 +48,15 @@ export default () => {
       input.classList.remove('is-valid');
       input.classList.add('is-invalid');
     }
-
-    sumbit.disabled = state.errors.name.length !== 0 || state.errors.email.length !== 0;
+    submit.disabled = state.errors.name.length !== 0 || state.errors.email.length !== 0;
   });
 
   form.addEventListener('input', (e) => {
     e.preventDefault();
     const targetName = e.target.name;
     const data = new FormData(form).get(targetName);
-    watchState.values[targetName] = data;
-    watchState.errors[targetName] = validateField(targetName, data);
+    watchedState.values[targetName] = data;
+    watchedState.errors[targetName] = validateField(targetName, data);
   });
 
   form.addEventListener('submit', (e) => {
